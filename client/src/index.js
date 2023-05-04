8259c98bf18e4f54b893c79bca61381d7ef7f52d
Zarrar Khan <zarrar.khan@pnnl.gov>
2023-05-04 15:22:38 -0400

    Updating 404 and index.html

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import {
   Navigation,
   Home,
